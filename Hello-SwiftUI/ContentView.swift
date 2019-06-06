a77d79784926af53239469dbc10e428951823e0e
Mohammad Azam <azamsharp@gmail.com>
2019-06-06 18:33:28 -0500

    Initial Commit

---
+++ Hello-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Hello-SwiftUI
+//
+//  Created by Naila Sheikh on 6/6/19.
+//  Copyright © 2019 Naila Sheikh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
